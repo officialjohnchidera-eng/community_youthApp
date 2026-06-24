b2b0355e9d3a85aa46c42866e2ff83a26c577d93
Chhidera <chipeter633@gmail.com>
2026-06-24 12:17:21 +0100

    Switch from SMTP to SendGrid API for email delivery

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -179,6 +179,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 
 # Cloudinary
 import cloudinary
